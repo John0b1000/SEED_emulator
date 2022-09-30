5e82e8508f884f1f70850911629b9fb6b410f53d
John Nori <johnlnori8@gmail.com>
2022-09-30 15:50:18 -0400

    Update seed_install_ros.sh

--- seed_install_ros.sh
+++ seed_install_ros.sh
@@ -8,7 +8,7 @@ add-apt-repository multiverse
 add-apt-repository restricted
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 apt install curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | apt-key add -
 apt update
 apt install ros-noetic-ros-base
 . /opt/ros/noetic/setup.bash
